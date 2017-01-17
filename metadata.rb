d4d9c0100df095e2c461f29c9c9ec45dd46485ae
praveen pulisetty <praveen pulisetty>
2017-01-17 20:01:56 +0530

    This is dockerconfig using chef

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
-name 'docker'
+name 'dockercb'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'all_rights'
@@ -13,3 +13,4 @@ version '0.1.0'
 # If you upload to Supermarket you should set this so your cookbook
 # gets a `View Source` link
 # source_url 'https://github.com/<insert_org_here>/docker' if respond_to?(:source_url)
+depends'docker', '~> 2.13.9'
